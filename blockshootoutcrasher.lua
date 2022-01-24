20c902acf938a73996cce893afe96c589981fdf7
stillnotgivingafuck <90551400+stillnotgivingafuck@users.noreply.github.com>
2022-01-24 23:40:41 +0000

    Create blockshootoutcrasher.lua

---
+++ blockshootoutcrasher.lua
@@ -0,0 +1,7 @@
+--[[server crasher for this shit game.
+Hold an AR out then execute kthx mwa]]--
+
+local hack = game:GetService("Players").LocalPlayer
+for i = 1,3000 do
+game:GetService("Players").LocalPlayer.Character.AR.Shell:FireServer(hack)
+end
