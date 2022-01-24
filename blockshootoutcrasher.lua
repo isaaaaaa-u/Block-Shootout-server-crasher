29f0fa1506f7f78f6a980a51596bd0a453a67959
stillnotgivingafuck <90551400+stillnotgivingafuck@users.noreply.github.com>
2022-01-24 23:41:20 +0000

    Update blockshootoutcrasher.lua

--- blockshootoutcrasher.lua
+++ blockshootoutcrasher.lua
@@ -1,5 +1,6 @@
 --[[server crasher for this shit game.
-Hold an AR out then execute kthx mwa]]--
+Hold an AR out then execute kthx mwa
+https://www.roblox.com/games/8199791819/Block-ShootOut-20K-UPDATED]]--
 
 local hack = game:GetService("Players").LocalPlayer
 for i = 1,3000 do
