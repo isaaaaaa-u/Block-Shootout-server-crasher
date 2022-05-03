8b910ea6e1739ec9e909c618f032f12dcf638d4c
stillnotgivingafuck <90551400+stillnotgivingafuck@users.noreply.github.com>
2022-05-03 12:13:25 +0100

    Update blockshootoutcrasher.lua

--- blockshootoutcrasher.lua
+++ blockshootoutcrasher.lua
@@ -2,7 +2,10 @@
 Hold an AR out then execute kthx mwa
 https://www.roblox.com/games/8199791819/Block-ShootOut-20K-UPDATED]]--
 
-local hack = game:GetService("Players").LocalPlayer
-for i = 1,3000 do
-game:GetService("Players").LocalPlayer.Character.AR.Shell:FireServer(hack)
+local LP = game:GetService'Players'.LocalPlayer
+local Tool = LP.Character.FindFirstChildOfClass'Tool'
+local Shell = Tool.Shell
+
+for _ = 1,3000 do
+  Shell:FireServer(LP)
 end
